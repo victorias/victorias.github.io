6b35432b74335152ba4d279d296640add8ab03d5
victoria <victoria.s.sun@gmail.com>
2019-10-04 12:50:15 -0700

    Updates

---
+++ precache-manifest.7d52faaf75dfb060d9ef1888faf0e0d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dd2adee72d90739b3a16b391967ad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efcf964ac931dabf1e50",
+    "url": "/static/css/main.d0028958.chunk.css"
+  },
+  {
+    "revision": "83c79ca04abe39d5c65a",
+    "url": "/static/js/2.69e5e37e.chunk.js"
+  },
+  {
+    "revision": "efcf964ac931dabf1e50",
+    "url": "/static/js/main.d6de5a9d.chunk.js"
+  },
+  {
+    "revision": "0b52182cf06712743464",
+    "url": "/static/js/runtime-main.8d5167c2.js"
+  },
+  {
+    "revision": "fd2210375d29eec188ed140cd5857e17",
+    "url": "/static/media/app.fd221037.svg"
+  },
+  {
+    "revision": "36dc0d4b94f7442f2ed65819ccf904d4",
+    "url": "/static/media/dress.36dc0d4b.svg"
+  },
+  {
+    "revision": "336f382b94e2ba008df0dafc3e7be670",
+    "url": "/static/media/hanger.336f382b.svg"
+  },
+  {
+    "revision": "ea3ce81f81aae3092f9aec6419de5a87",
+    "url": "/static/media/package.ea3ce81f.svg"
+  },
+  {
+    "revision": "c3be15f85abdb63cbc4ccec309f526c8",
+    "url": "/static/media/us.c3be15f8.jpg"
+  }
+]);
